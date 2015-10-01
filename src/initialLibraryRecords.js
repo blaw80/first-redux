dec8569b59ffee8e4155713a5ee7fbe1404e4c4f
Konstantin Grushetsky <toxickg@openmailbox.org>
2015-10-02 01:58:05 +0300

    Switched to 'redux-actions' for actions creation and handling
    Extracted initial library state into separate module.

---
+++ src/initialLibraryRecords.js
@@ -0,0 +1,87 @@
+export default [{
+    "_id": "55cf8c9462ca996f34dada98",
+    "songtitle": "You Gonna Quit Me",
+    "artist": "Blind Blake",
+    "url": "https://archive.org/download/Blind_Blake-You_Gonna_Quit/Blind_Blake--You_Gonna_Quit_Me_Blues.mp3"
+  }, {
+    "_id": "55cf8f5b62ca996f34dada99",
+    "songtitle": "Easy Rider",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJeffersonMp3AudioSongs/blindlemonjefferson-EasyRiderBlues.mp3"
+  }, {
+    "_id": "55cf8f7f62ca996f34dada9a",
+    "songtitle": "Police Dog Blues",
+    "artist": "Blind Blake",
+    "url": "https://archive.org/download/blind_blake_police_dog_blues/blind_blake_police_dog_blues.mp3"
+  }, {
+    "_id": "55cf936662ca996f34dada9b",
+    "songtitle": "How Long How Long",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJeffersonMp3AudioSongs/blindlemonjefferson-HowLongHowLong.mp3"
+  }, {
+    "_id": "55cf943e62ca996f34dadaa0",
+    "songtitle": "Black Snake Moan",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/04%20That%20Black%20Snake%20Moan.mp3"
+  }, {
+    "_id": "55cf943f62ca996f34dadaa2",
+    "songtitle": "Stocking Feet Blues",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/03%20Stocking%20Feet%20Blues.mp3"
+  }, {
+    "_id": "55cf943f62ca996f34dadaa3",
+    "songtitle": "Shuckin Sugar Blues",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/05%20Shuckin'%20Sugar%20Blues.mp3"
+  }, {
+    "_id": "55cf943f62ca996f34dadaa7",
+    "songtitle": "Broke and Hungry",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/08%20Broke%20And%20Hungry.mp3"
+  }, {
+    "_id": "55cf943f62ca996f34dadaa5",
+    "songtitle": "Rabbit Foot Blues",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/06%20Rabbit%20Foot%20Blues.mp3"
+  }, {
+    "_id": "55cf943f62ca996f34dadaa6",
+    "songtitle": "Bad Luck Blues",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/07%20Bad%20Luck%20Blues.mp3"
+  }, {
+    "_id": "55cf965d62ca996f34dadaa8",
+    "songtitle": "Chock House Blues",
+    "artist": "Blind Lemon Jefferson",
+    "url": "https://archive.org/download/BlindLemonJefferson-TheEssential/02%20Chock%20House%20Blues.mp3"
+  }, {
+    "_id": "55d39f1431d767d5711df94e",
+    "songtitle": "Drunken Hearted Man",
+    "artist": "Robert Johnson",
+    "url": "https://archive.org/download/RobertJohnsonMp3AudioSongs/robertjohnson-DrunkenHeartedMan.mp3"
+  }, {
+    "_id": "55d39f4731d767d5711df94f",
+    "songtitle": "Kind Hearted Woman Blues",
+    "artist": "Robert Johnson",
+    "url": "https://archive.org/download/RobertJohnsonMp3AudioSongs/robertjohnson-KindheartedWomanBlues.mp3"
+  }, {
+    "_id": "55d39f8e31d767d5711df950",
+    "songtitle": "Little Queen of Spades",
+    "artist": "Robert Jonson",
+    "url": "https://archive.org/download/RobertJohnsonMp3AudioSongs/robertjohnson-LittleQueenOfSpades.mp3"
+  }, {
+    "_id": "55d39fc831d767d5711df951",
+    "songtitle": "Malted Milk",
+    "artist": "Robert Johnson",
+    "url": "https://archive.org/download/RobertJohnsonMp3AudioSongs/robertjohnson-MaltedMilk.mp3"
+  }, {
+    "_id": "55d3a07731d767d5711df952",
+    "songtitle": "34 blues",
+    "artist": "Charlie Patton",
+    "url": "https://archive.org/download/CharleyPatton55Songs/Charley%20Patton%20-%20'34%20Blues%201934.mp3"
+  }, {
+    "_id": "55d3a0bd31d767d5711df953",
+    "songtitle": "Spoonful Blues",
+    "artist": "Charlie Patton",
+    "url": "https://archive.org/download/CharleyPatton55Songs/Charley%20Patton%20-%20A%20Spoonful%20Blues%201929.mp3"
+  }
+]
